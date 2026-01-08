84665958465516d662a88c608fbdc41189c4234b
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-08 13:42:09 -0300

    Remove "use client" from new-list-button component

--- app/ui/new-list-button.tsx
+++ app/ui/new-list-button.tsx
@@ -1,17 +1,11 @@
-"use client";
-
 import styles from "./new-list-button.module.css";
-import { List, ListPlus, Plus } from "lucide-react";
+import { ListPlus, Plus } from "lucide-react";
 
 interface NewListButtonProps {}
 
 export default function NewListButton() {
   return (
-    <button
-      className={styles.newListButton}
-      onClick={() => console.log("Adding new task")}
-      aria-label="New List"
-    >
+    <button className={styles.newListButton} aria-label="New List">
       <div className={styles.newListButtonTitleWrapper}>
         <Plus color="blue" />
         <p className={styles.newListButtonText}>New List</p>
