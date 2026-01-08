d4456e1133b2a1736d9bb5ff7cc23b09b7775d02
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-08 13:31:17 -0300

    Format code with prettier

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -4,14 +4,14 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function loginAction(formData: FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
-    const cookieStore = await cookies();
-    cookieStore.set("session_token", "mock_user_id", {
-        path: "/",
-        maxAge: 3600,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-    });
-    redirect("/");
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const cookieStore = await cookies();
+  cookieStore.set("session_token", "mock_user_id", {
+    path: "/",
+    maxAge: 3600,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  });
+  redirect("/");
 }
