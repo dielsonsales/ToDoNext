56d5f7a9d346d73fb5da98d1140e7ab96cb4cd93
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-10 21:06:56 -0300

    Create component for wrapping pages inside Konsta's <App>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import KonstaProvider from "./konsta-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <KonstaProvider>
+          {children}
+        </KonstaProvider>
+      </body>
     </html>
   );
 }
