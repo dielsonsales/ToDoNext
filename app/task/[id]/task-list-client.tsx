58c366d0e2b9faea7ccab8ed2229ce6688029557
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-12 23:54:53 -0300

    Adjust visual of the bottom input in the TaskListClient

--- app/task/[id]/task-list-client.tsx
+++ app/task/[id]/task-list-client.tsx
@@ -5,6 +5,7 @@ import { Task } from "@/app/lib/definitions";
 import TaskItem from "@/app/ui/task-item";
 import { List, ListInput, Navbar, NavbarBackLink, Page } from "konsta/react";
 import { createTaskAction } from "./actions";
+import { Plus } from "lucide-react";
 
 interface TaskListClientProps {
   tasks: Task[];
@@ -45,15 +46,20 @@ export default function TaskListClient({
         left={<NavbarBackLink href="/" />}
       />
       <List insetIos className="flex-1 overflow-y-auto">{taskComponents}</List>
-      <footer className="shrink-0 bg-white pb-safe border-t">
-        <form ref={formRef} action={clientAction}>
-          <ListInput
-            name="title"
-            type="text"
-            placeholder="Add a Task"
-            className="!mb-0"
-          />
-        </form>
+      <footer className="shrink-0 pb-safe px-2 mb-4">
+        <div className="bg-black/20 rounded-2xl p-2 backdrop-blur-md shadow-lg">
+          <form ref={formRef} action={clientAction}>
+            <ListInput
+              name="title"
+              type="text"
+              placeholder="Add a Task"
+              media={<Plus className="text-white/70" size={20} />}
+              className="!bg-white/20 rounded-lg !border-none"
+              inputClassName="!text-white placeholder:!text-white/50"
+              outline={false}
+            />
+          </form>
+        </div>
       </footer>
     </Page>
   );
