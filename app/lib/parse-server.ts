7409289616bc2a9d62b048e4ad191869152a3ed1
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-23 19:14:11 -0300

    Add current user to application context

--- app/lib/parse-server.ts
+++ app/lib/parse-server.ts
@@ -1,6 +1,25 @@
 import Parse from "parse/node";
 
-Parse.initialize(process.env.PARSE_APP_ID!, process.env.PARSE_JS_KEY!);
-Parse.serverURL = "https://parseapi.back4app.com";
+// Ensures Parse is only initialized once
+if (!Parse.applicationId) {
+  Parse.initialize(process.env.PARSE_APP_ID!, process.env.PARSE_JS_KEY!);
+  Parse.serverURL = "https://parseapi.back4app.com";
+}
+
+export async function getParseUser(token: string): Promise<any | null> {
+  try {
+    const query = new Parse.Query(Parse.User);
+    const user = await query.first({ sessionToken: token });
+
+    if (!user) return null;
+
+    const userJSON = user.toJSON();
+    delete userJSON.sessionToken;
+    return userJSON;
+  } catch (error) {
+    console.error("Parse Auth error:", error);
+    return null;
+  }
+}
 
 export default Parse;
