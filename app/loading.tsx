cdf528619221caf8694b50132d7d5eebd72d2fd4
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-23 22:58:28 -0300

    Create loading page

---
+++ app/loading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { List, ListItem, Page } from "konsta/react";
+
+export default function Loading() {
+  return (
+    <Page>
+      <div className="p-4 bg-gray-50 animate-pulse">
+        <div className="h-8 w-32 bg-gray-200 rounded mb-4" />
+      </div>
+      <List insetIos className="mt-0">
+        {[...Array(8)].map((_, i) => (
+          <ListItem
+            key={i}
+            title={<div className="h-4 w-48 bg-gray-200 rounded animate-pulse" />}
+            media={<div className="h-6 w-6 bg-gray-300 rounded-full animate-pulse" />}
+          />
+        ))}
+      </List>
+    </Page>
+  );
+}
